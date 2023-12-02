03aa7f54b44fb77140e214b8ef3b656007fd560b
John  Boulanger <boulan23@remote.ecf.utoronto.ca>
2023-12-02 16:26:15 -0500

    added commands that send a message to client asking to join, create, leave, list, quit, and logout

--- client.c
+++ client.c
@@ -16,7 +16,7 @@
 
 // 8 user input commands for client to implement
 /*
-1. /login clientID, pwd, serverIP, serverPort - log into server
+1. /login ID, pwd, serverIP, serverPort - log into server
 2. /logout - exit server
 3. /joinsession sessionID - join a specific session
 4. /leaveession - leave the session
@@ -26,16 +26,26 @@
 8. <text> - send text while in a session 
 */
 
-const int BUFFER_SZ = 1000;
+const int BUFFER_SZ = 1024;
 const int PACKET_SZ = 2008; // 4 + 4 + 1000 + 1000
 
 char inputBuf[1000];
 
+struct clientInfo{
+    char *clientID;
+    char *sessionID;
+    int sockfd;
+};
+
+struct clientInfo *client;
+
 void login(int sockfd, char *inputSlice);
-void logout();
-void joinSess();
-void leaveSess();
-void list();
+void logout(int sockfd);
+void joinSess(int sockfd, char *inputSlice);
+void leaveSess(int sockfd);
+void createSess(int sockfd, char *inputSlice);
+void list(int sockfd);
+void quit(int sockfd);
 void text();
 
 
@@ -49,7 +59,7 @@ int main(){
         // scanf("%[^\n]s",inputBuf);
         fgets(inputBuf, BUFFER_SZ - 1, stdin);
         inputBuf[strlen(inputBuf) - 1] = '\0'; //set end of inputBuf to null instead of \n    
-        
+        client = malloc(sizeof(struct clientInfo));
         inputSlice = inputBuf;
         if(*inputSlice == '\0') continue;
         
@@ -65,18 +75,23 @@ int main(){
             
         }else if(strcmp(inputSlice,"/joinsession") == 0){
             printf("join\n");
+            joinSess(sockfd, inputSlice);
             
         }else if(strcmp(inputSlice,"/leavesession") == 0){
             printf("leave\n");
+            leaveSess(sockfd);
             
         }else if(strcmp(inputSlice,"/createsession") == 0){
             printf("create\n");
+            createSess(sockfd, inputSlice);
             
         }else if(strcmp(inputSlice,"/list") == 0){
             printf("list\n");
+            list(sockfd);
             
         }else if(strcmp(inputSlice,"/quit") == 0){
             printf("quit\n");
+            quit(sockfd);
 
             break;
             
@@ -107,9 +122,11 @@ void login(int sockfd, char *inputSlice){
 
     //put client into database for the server
 
-    char *clientID, *pwd, *serverIP, *serverPort;
+    char *ID, *pwd, *serverIP, *serverPort;
     inputSlice = strtok(NULL, " ");
-    clientID = inputSlice;
+    ID = inputSlice;
+    // save id into global variable
+    client->clientID = ID;
     inputSlice = strtok(NULL, " ");
     pwd = inputSlice;
     inputSlice = strtok(NULL, " ");
@@ -117,7 +134,7 @@ void login(int sockfd, char *inputSlice){
     inputSlice = strtok(NULL, "\0");
     serverPort = inputSlice;
 
-    printf("split msg: %s | %s | %s | %s\n",clientID,pwd,serverIP,serverPort);
+    printf("split msg: %s | %s | %s | %s\n",ID,pwd,serverIP,serverPort);
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr(serverIP); //take input from user
@@ -132,11 +149,11 @@ void login(int sockfd, char *inputSlice){
     struct message *packet = malloc(PACKET_SZ);
     packet->type = LOGIN; //assign packet type
 
-    for(int i = 0; i < strlen(clientID); i++) packet->source[i] = clientID[i]; //assign packet source
+    for(int i = 0; i < strlen(ID); i++) packet->source[i] = ID[i]; //assign packet source
 
     //assign packet data
     char *data = malloc(1000);
-    sprintf(data,"%s %s %s %s",clientID,pwd,serverIP,serverPort);
+    sprintf(data,"%s %s %s %s",ID,pwd,serverIP,serverPort);
     for(int j = 0; j < 1000; j++) packet->data[j] = data[j];
 
     packet->size = strlen(data); //assign packet data length
@@ -149,6 +166,161 @@ void login(int sockfd, char *inputSlice){
     
     write(sockfd,packetSend, strlen(packetSend));
 
-    //now need to wait for login ACK or NAK from server before allowing a new command to go through
-    // printf("program crash?\n");
+    char buff[BUFFER_SZ];
+    bzero(buff, sizeof(buff));
+
+    read(sockfd, buff, sizeof(buff));
+    // parse the buffer message from the server
+}
+
+void joinSess(int sockfd, char *inputSlice){
+    char buff[BUFFER_SZ];
+    bzero(buff, sizeof(buff));
+    // extract the session ID from the user (doesnt have to be a number)
+    char *sessionID;
+    struct message *packet = malloc(PACKET_SZ);
+    inputSlice = strtok(NULL, " ");
+    sessionID = inputSlice;
+    client->sessionID = sessionID;
+    // format the message
+    packet->type = JN_ACK;
+    packet->size = strlen(sessionID);
+    for(int i = 0; i < strlen(client->clientID); i++) packet->source[i] = client->clientID[i];
+    for(int i = 0; i < strlen(sessionID); i++) packet->data[i] = sessionID[i];
+
+    // send message as one contiguous string
+    char *packetSend = malloc(PACKET_SZ);
+    sprintf(packetSend, "%d,%d,%s,%s",packet->type, packet->size, packet->source, packet->data);
+    printf("printing packet to send: %s\n",packetSend);
+
+    // send request to join session to server
+    write(sockfd,packetSend, strlen(packetSend));
+
+    // wait for ACK / NACK
+    read(sockfd, buff, sizeof(buff));
+    return;
+}
+
+void createSess(int sockfd, char *inputSlice){
+    char buff[BUFFER_SZ];
+    bzero(buff, sizeof(buff));
+    // extract the session ID from the user (doesnt have to be a number)
+    char *sessionID;
+    struct message *packet = malloc(PACKET_SZ);
+    inputSlice = strtok(NULL, " ");
+    sessionID = inputSlice;
+    client->sessionID = sessionID;
+    // format the message
+    packet->type = NEW_SESS;
+    packet->size = strlen(sessionID);
+    for(int i = 0; i < strlen(client->clientID); i++) packet->source[i] = client->clientID[i];
+    for(int i = 0; i < strlen(sessionID); i++) packet->data[i] = sessionID[i];
+
+    // send message as one contiguous string
+    char *packetSend = malloc(PACKET_SZ);
+    sprintf(packetSend, "%d,%d,%s,%s",packet->type, packet->size, packet->source, packet->data);
+    printf("printing packet to send: %s\n",packetSend);
+    
+    // send request to create the session to the server
+    write(sockfd,packetSend, strlen(packetSend));
+
+    // wait for ACK/NACK
+    read(sockfd, buff, sizeof(buff));
+    // parse the buffer message from the server
+
+    return;
+}
+
+void leaveSess(int sockfd){
+    char buff[BUFFER_SZ];
+    bzero(buff, sizeof(buff));
+    struct message *packet = malloc(PACKET_SZ);
+    // format the message
+    packet->type = LEAVE_SESS;
+    packet->size = 0;
+    for(int i = 0; i < strlen(client->clientID); i++) packet->source[i] = client->clientID[i];
+    packet->data[0] = '\0';
+    // send message as one contiguous string
+    char *packetSend = malloc(PACKET_SZ);
+    sprintf(packetSend, "%d,%d,%s,%s",packet->type, packet->size, packet->source, packet->data);
+    printf("printing packet to send: %s\n",packetSend);
+
+    // send request to create the session to the server
+    write(sockfd,packetSend, strlen(packetSend));
+
+    // wait for ACK/NACK
+    read(sockfd, buff, sizeof(buff));
+    // parse the buffer message from the server
+
+    return;
+}
+
+void list(int sockfd){
+    char buff[BUFFER_SZ];
+    bzero(buff, sizeof(buff));
+    struct message *packet = malloc(PACKET_SZ);
+    // format the message
+    packet->type = QUERY;
+    packet->size = 0;
+    for(int i = 0; i < strlen(client->clientID); i++) packet->source[i] = client->clientID[i];
+    packet->data[0] = '\0';
+    // send message as one contiguous string
+    char *packetSend = malloc(PACKET_SZ);
+    sprintf(packetSend, "%d,%d,%s,%s",packet->type, packet->size, packet->source, packet->data);
+    printf("printing packet to send: %s\n",packetSend);
+
+    // send request to create the session to the server
+    write(sockfd,packetSend, strlen(packetSend));
+
+    // wait for ACK/NACK
+    read(sockfd, buff, sizeof(buff));
+    // parse the buffer message from the server
+
+    return;
+}
+
+void quit(int sockfd){
+    char buff[BUFFER_SZ];
+    bzero(buff, sizeof(buff));
+    struct message *packet = malloc(PACKET_SZ);
+    // format the message
+    packet->type = EXIT;
+    packet->size = 0;
+    for(int i = 0; i < strlen(client->clientID); i++) packet->source[i] = client->clientID[i];
+    packet->data[0] = '\0';
+    // send message as one contiguous string
+    char *packetSend = malloc(PACKET_SZ);
+    sprintf(packetSend, "%d,%d,%s,%s",packet->type, packet->size, packet->source, packet->data);
+    printf("printing packet to send: %s\n",packetSend);
+
+    // send quit request
+    write(sockfd,packetSend, strlen(packetSend));
+
+    // wait for ack?
+    read(sockfd, buff, sizeof(buff));
+    // parse the buffer message from the server
+    return;
+}
+
+void logout(int sockfd){
+    char buff[BUFFER_SZ];
+    bzero(buff, sizeof(buff));
+    struct message *packet = malloc(PACKET_SZ);
+    // format the message
+    packet->type = EXIT;
+    packet->size = 0;
+    for(int i = 0; i < strlen(client->clientID); i++) packet->source[i] = client->clientID[i];
+    packet->data[0] = '\0';
+    // send message as one contiguous string
+    char *packetSend = malloc(PACKET_SZ);
+    sprintf(packetSend, "%d,%d,%s,%s",packet->type, packet->size, packet->source, packet->data);
+    printf("printing packet to send: %s\n",packetSend);
+
+    // send quit request
+    write(sockfd,packetSend, strlen(packetSend));
+
+    // wait for ack?
+    read(sockfd, buff, sizeof(buff));
+    // parse the buffer message from the server
+    return;
 }
